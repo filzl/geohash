036432e7b7f965cc65efbc415c3dc5ba00cf41e2
filzl <133703384+filzl@users.noreply.github.com>
2023-05-16 13:35:46 +0200

    Update to main.c

--- main.c
+++ main.c
@@ -15,29 +15,38 @@ int main(int argc, char **args){
 	east = 2.294;
 	
 	hashlen = 6;
-		
-	if(argc>4){
+
+	if(argc == 2)
+		hashlen = atoi(args[1]);
+	
+	else if(argc >= 3){
 		north = strtod(args[1], NULL);
 		west = strtod(args[2], NULL);
-		south = strtod(args[3], NULL);
-		east = strtod(args[4], NULL);
-	}else if(argc>1)
-		hashlen = atoi(args[1]);
 
-	if(argc>5)
-		hashlen = atoi(args[5]);
-		
+		if(argc == 4)
+			hashlen = atoi(args[3]);
+
+		if(argc >= 5){
+			south = strtod(args[3], NULL);
+			east = strtod(args[4], NULL);
+			if(argc >= 6)
+				hashlen = atoi(args[5]);
+		}else{
+			south = strtod(args[1], NULL);
+			east = strtod(args[2], NULL);
+		}
+	}
 
 	hashcount = geohash_region(north, west, south, east, hashlen, &hashlist);
 
-	printf("(");
 	for(i = 0; i < hashcount; i++){
 		if(i)
 			printf(", ");
 		printf("%s", hashlist[i]);
 		free(hashlist[i]);
 	}
-	printf(")\n");
+
+	printf("\n");
 	
 	free(hashlist);
 
